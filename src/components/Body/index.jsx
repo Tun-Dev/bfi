aceb7c1428c902aa77b75c10e11569f07f1b0f53
Douye Tungbulu <tungbulupaul@gmail.com>
2022-07-28 02:30:53 +0100

    images

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -11,24 +11,24 @@ const Body = () => {
         <div className="inner">
           <div className="card-section">
             <Card
-              image={interior}
+              image="https://res.cloudinary.com/dcoqt2wpo/image/upload/v1658971739/interior_ijlrbb.png"
               title="Interior Architecture"
               text="With beautiful display options, from hutch and open shelving to filing cabinet, we have the solution for your desired office space."
               button={true}
             />
             <Card
-              image={space}
+              image="https://res.cloudinary.com/dcoqt2wpo/image/upload/v1658971739/space_e33i7a.png"
               title="Space Decor"
               text="We offer a large selection of high quality interior office space decor to help you truly create the look that is right for you."
               button={true}
             />
             <Card
-              image={luxury}
+              image="https://res.cloudinary.com/dcoqt2wpo/image/upload/v1658971740/luxury_dqnnk2.png"
               title="Luxury Furniture"
               text="We curated a collection of inspirational, premium chairs and desks that elevate your space"
             />
             <Card
-              image={luxury2}
+              image="https://res.cloudinary.com/dcoqt2wpo/image/upload/v1658971785/luxury2_kp531x.png"
               title="Luxury Furniture"
               text="We curated a collection of inspirational, premium chairs and desks that elevate your space"
             />
